46bdfa886d75068652cc4dca2f44847a674bcd4c
Gabriel Nakamoto <gabriel@nakamoto.ca>
2025-08-22 18:16:04 -0400

    Added api warning

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import dearpygui.dearpygui as dpg
+import os
 from entity import Entity
 from paperclient import PaperClient
 from arxivclient import ArxivClient
@@ -16,6 +17,11 @@ TODO:
 """
 
 def main():
+	if os.environ.get("DAND_TOKEN") == None:
+		print("Dandelion API key required")
+		print("Get one at https://dandelion.eu/ and set DAND_TOKEN env key")
+		return
+
 	dpg.create_context()
 
 	dpg.create_viewport(title='Recurse Paper', width=1600, height=800)
